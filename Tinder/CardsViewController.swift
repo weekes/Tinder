f761bed83366c15793f3385501517546de662123
Marcel Weekes <weekes@gmail.com>
2016-03-02 19:27:40 -0800

    Milestone 2

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class CardsViewController: UIViewController {
 
+    @IBOutlet weak var navBarImageView: UIImageView!
+    @IBOutlet weak var profilePicImageView: UIImageView!
+    @IBOutlet weak var actionButtonsImageView: UIImageView!
+    
+    var originalProfileCenter: CGPoint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,25 @@ class CardsViewController: UIViewController {
     }
 
 
+    @IBAction func onProfilePanGesture(sender: UIPanGestureRecognizer) {
+        var point = sender.locationInView(view)
+        var velocity = sender.velocityInView(view)
+        var translation = sender.translationInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+//            print("Gesture began at: \(point)")
+            originalProfileCenter = self.view.center
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            sender.view?.center.x = originalProfileCenter.x + translation.x
+//            self.view.center.x = originalProfileCenter.x + translation.x
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            print("Gesture ended at: \(point)")
+        }
+    }
+    
 }
 
